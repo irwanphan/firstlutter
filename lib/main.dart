bdece8773891e466d66073ba9e8e951dd6b06c0a
irwanphan <irwanphan@gmail.com>
2020-06-03 21:11:56 +0700

    Hello World with material style

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Text('Default text here...'),
+      ),
+    );
   }
 }
