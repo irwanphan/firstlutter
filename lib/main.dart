c01e24e978ec1f6a0eaf273b46ca6f76d574ec09
irwanphan <irwanphan@gmail.com>
2020-06-03 21:40:54 +0700

    function when button onPressed

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: Text('Hello World'),
-        backgroundColor: Colors.purple,
-      ),
-      body: Text('Default text here...'),
+    var questions = [
+      'What\'s your fave color?', 
+      'Who a ya?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World'),
+          backgroundColor: Colors.indigoAccent,
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'), 
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
